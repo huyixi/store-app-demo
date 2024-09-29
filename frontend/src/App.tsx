b4b5955bb81902c3c907da7779028089e534504d
huyixi <hi@huyixi.com>
2024-09-29 18:03:23 +0800

    feat: add tailwindcss

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import './styles/App.css';
+import './styles/main.css';
+
+const App = () => {
+  return (
+    <div className="content">
+      <h1>Rsbuild with React</h1>
+      <p>Start building amazing things with Rsbuild.</p>
+      <h1 className="text-3xl text-red-500 font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  );
+};
+
+export default App;
